365410117a4d19657fb9522ba7c31b3626b9568d
alimoradi <alimoradi7am@gmail.com>
2022-07-27 11:07:14 +0430

    addUserClass

---
+++ src/main/java/entiry/User.java
@@ -0,0 +1,13 @@
+package entiry;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String username;
+    private String nationalCode;
+    private Date birthDay;
+    private  int password;
+
+
+}
