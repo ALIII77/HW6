56f0f269948b50894018adace7f6470f4a471850
alimoradi <alimoradi7am@gmail.com>
2022-07-27 11:32:35 +0430

    addUserClass

--- src/main/java/entiry/User.java
+++ src/main/java/entiry/User.java
@@ -9,5 +9,56 @@ public class User {
     private Date birthDay;
     private  int password;
 
+    public User ()
+        {
 
+        }
+
+    public User(int id, String username, String nationalCode, Date birthDay, int password) {
+        this.id = id;
+        this.username = username;
+        this.nationalCode = nationalCode;
+        this.birthDay = birthDay;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
 }
